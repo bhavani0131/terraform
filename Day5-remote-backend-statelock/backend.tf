10b3d53ab8d13987da72e1e777d3a3de87019ef5
Durga Bhavani <durgabhavaniv77@gmail.com>
2026-06-04 18:35:26 +0530

    new updates

--- Day5-remote-backend-statelock/backend.tf
+++ Day5-remote-backend-statelock/backend.tf
@@ -2,7 +2,12 @@ terraform {
   backend "s3" {
     bucket = "bhavanistatebucket"
     key = "terraform.tfstate"
+    dynamodb_table = "bhavani"
+    encrypt = true
     use_lockfile = true #native locking process to prevent cocurrent state modification
     region = "us-east-1"
   }
 }
+#supports latest version >=1.10
+#<1.10 we can use dynamodb for state locking as well, but s3 native locking is more efficient and cost effective
+#State lockfile: Terraform acquires a state lock to protect the state from being written by multiple users at the same. Please resolve the above issue and
--- Day5-remote-backend-statelock/main.tf
+++ Day5-remote-backend-statelock/main.tf
@@ -4,3 +4,10 @@ resource "aws_vpc" "name" {
     Name = "terraform"
   }
 }
+resource "aws_subnet" "name" {
+  vpc_id = aws_vpc.name.id
+  cidr_block = "10.0.0.0/26"
+  tags = {
+    Name = "terraform"
+  }
+}
